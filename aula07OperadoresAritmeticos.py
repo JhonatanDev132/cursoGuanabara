09fed1923e77d2f6eb909f3a9dd0674af9fec8ca
Jhonatan Vinicius <jhonnytrabalho2005@gmail.com>
2024-07-17 14:42:51 -0300

    trocando nomes para ter um padrão

---
+++ aula07OperadoresAritmeticos.py
@@ -0,0 +1,14 @@
+# Ordem de Precedência
+
+# 1. () Parenteses
+
+# 2. ** Potenciação // 5**2 = 25
+
+# 3. // Divisão inteira // 5//2 = 2
+#    % Resto de divisão // 5%2 = 1
+#    * Multiplicação // 5*2 = 10
+#    / divisão // 5/2 = 2.5
+
+# 4. + Adição // 5+2 = 7
+#    - Subtração // 5-2 = 3
+
