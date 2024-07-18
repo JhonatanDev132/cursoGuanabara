4cb88b6d39f6093155eb1d8ea3bf3d037d11de18
Jhonatan Vinicius <jhonnytrabalho2005@gmail.com>
2024-07-18 14:08:32 -0300

    parede feita chefia 👍

--- aula07OperadoresAritmeticos.py
+++ aula07OperadoresAritmeticos.py
@@ -23,12 +23,28 @@
 #print("A soma é {}, a multiplicação é {}, e a divisão é {:.3f} ".format(s, m, d))
 #print("Divisão inteira {}, e potência {}".format(di, e))
 
-numero = int(input("Digite um número para que a tabuada seja feita: "))
-contador = 0
+#numero = int(input("Digite um número para que a tabuada seja feita: "))
+#contador = 0
 
-print("A tabuada do número {} é: ".format(numero))
+#print("A tabuada do número {} é: ".format(numero))
 
-while numero <= 10 :
-    num = numero * contador
-    print("{} x {} = {}".format(numero, contador, num))
-    contador += 1
+#while contador <= 10 :
+#    num = numero * contador
+#    print("{} x {} = {}".format(numero, contador, num))
+#    contador += 1
+
+
+#carteira = float(input("Quanto dinheiro você tem na carteira? "))
+#precoReal = 3.27
+
+#carteiraDolar = carteira / precoReal
+
+#print("Com R${:.2f} Você pode comprar ${:.2f} doláres.".format(carteira, carteiraDolar))
+
+largura = float(input("Escreva a largura da parede: "))
+altura = float(input("Escreva a altura da parede: "))
+
+area = largura * altura
+tinta = area / 2
+
+print("Sua parede tem uma area de {}m², e será necessário {}l de tinta para pintar ela.".format(area, tinta))
