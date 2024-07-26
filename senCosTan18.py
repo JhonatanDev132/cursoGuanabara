44001e1f2e5be446812fb4f9697413433d680113
Jhonatan Vinicius <jhonnytrabalho2005@gmail.com>
2024-07-26 14:55:13 -0300

    Update senCosTan18.py

--- senCosTan18.py
+++ senCosTan18.py
@@ -5,6 +5,6 @@ seno = math.sin(math.radians(an))
 cos = math.cos(math.radians(an))
 tan = math.tan(math.radians(an))
 
-print("O cosseno do ângulo {} é: {:.2f}".format(an, cos))
 print("O seno do ângulo {} é: {:.2f}".format(an, seno))
+print("O cosseno do ângulo {} é: {:.2f}".format(an, cos))
 print("A tangente do ângulo {} é: {:.2f}".format(an, tan))
